1649f538866f4fbf9cd0b658eeea30633ed516ca
jadeprogramma <jadeprogramma@proton.me>
2026-06-09 20:06:51 -0400

    fixed download buttons.

--- index.js
+++
@@ -1,9 +0,0 @@
-function downloadZIP(url) {
-    const filename = url.split("\/")[1];
-    const downloadLink = document.createElement("a");
-
-    downloadLink.href = url;
-    downloadLink.download = filename;
-
-    downloadLink.click();
-}
